b68cb2df51b2db74e7f8d8bea8f8bc7c52c42ab2
cherrydub <cherrydub@gmail.com>
2023-05-25 10:31:03 +0100

    updating favicon

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import BottomDesktopBar from "./BottomDesktopBar";
 import LeftDesktopNav from "./LeftDesktopNav";
 import MainDesktop from "./MainDesktop";
-// import "98.css";
+
 
 export default function Home() {
   return (
