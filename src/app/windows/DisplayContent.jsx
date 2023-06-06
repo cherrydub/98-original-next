2de63d7166fc6597e1669e4442bf2da84a571863
cherrydub <cherrydub@gmail.com>
2023-06-06 19:50:31 +0100

    added wallpapers and mobile wallpapers

--- src/app/windows/DisplayContent.jsx
+++ src/app/windows/DisplayContent.jsx
@@ -11,18 +11,41 @@ export default function DisplayContent({
 
   return (
     <div>
-      <p>Update background:</p>
+      <p>Desktop wallpapers:</p>
       <select
         className="pr-5 pl-2"
         value={backgroundChoice}
         onChange={handleBackgroundChange}
       >
         <option value="background-default">Default: Green</option>
+
+        <option value="background-98-sky-logo">98-Sky</option>
+        <option value="background-98-cougar">98-Cougar</option>
+        <option value="background-98-leopard">98-Leopard</option>
+        <option value="background-98-space">98-Space</option>
+        <option value="background-98-ocean">98-Ocean</option>
+        <option value="background-98-computer">98-Computer</option>
+        <option value="background-xp-classic">XP-Classic</option>
+
+        {/* <option value="background-pepe1">Pepe 1</option>
+        <option value="background-pepe3">Pepe 2</option>
+        <option value="background-ashpika">Pokemon</option>
+        <option value="background-dbz1">DragonBall Z</option>
+        <option value="background-robocop">Robocop</option> */}
+      </select>
+      <p>Mobile wallpapers:</p>
+      <select
+        className="pr-5 pl-2"
+        value={backgroundChoice}
+        onChange={handleBackgroundChange}
+      >
+        <option value="background-default">Default: Green</option>
+
+        <option value="background-pepe1">Pepe 1</option>
+        <option value="background-pepe3">Pepe 2</option>
         <option value="background-ashpika">Pokemon</option>
         <option value="background-dbz1">DragonBall Z</option>
         <option value="background-robocop">Robocop</option>
-        <option value="background-pepe1">Pepe 1</option>
-        <option value="background-pepe3">Pepe 2</option>
       </select>
       {/* <p>
         Add your own URL background: <input type="url" />
