0c71dc3efce9e79d7a185b3a6138c5b6f3024251
cherrydub <cherrydub@gmail.com>
2023-06-09 14:59:49 +0100

    commented out some wallpapers, added pepe

--- src/app/windows/DisplayContent.jsx
+++ src/app/windows/DisplayContent.jsx
@@ -45,12 +45,14 @@ export default function DisplayContent({
           </>
         ) : (
           <>
-            <option value="background-98-sky-logo">98-Sky</option>
+            <option value="background-pepe1">Pepe 1</option>
+            <option value="background-pepe3">Pepe 2</option>
+            {/* <option value="background-98-sky-logo">98-Sky</option> */}
             <option value="background-98-cougar">98-Cougar</option>
-            <option value="background-98-leopard">98-Leopard</option>
+            {/* <option value="background-98-leopard">98-Leopard</option>
             <option value="background-98-space">98-Space</option>
             <option value="background-98-ocean">98-Ocean</option>
-            <option value="background-98-computer">98-Computer</option>
+            <option value="background-98-computer">98-Computer</option> */}
             <option value="background-xp-classic">XP-Classic</option>
           </>
         )}
