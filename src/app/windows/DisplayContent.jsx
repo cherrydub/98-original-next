0953cdac7536daa4088ce26e74d327a51a4d1294
cherrydub <cherrydub@gmail.com>
2023-08-14 17:28:37 +0100

    default background

--- src/app/windows/DisplayContent.jsx
+++ src/app/windows/DisplayContent.jsx
@@ -33,7 +33,7 @@ export default function DisplayContent({
         value={backgroundChoice}
         onChange={handleBackgroundChange}
       >
-        <option value="background-default">Default: Green</option>
+        <option value="background-pepe3">Pepe 2</option>
 
         {isMobile ? (
           <>
@@ -42,18 +42,20 @@ export default function DisplayContent({
             <option value="background-ashpika">Pokemon</option>
             <option value="background-dbz1">DragonBall Z</option>
             <option value="background-robocop">Robocop</option>
+            <option value="background-default">Classic: Green</option>
           </>
         ) : (
           <>
             <option value="background-pepe1">Pepe 1</option>
             <option value="background-pepe3">Pepe 2</option>
+            <option value="background-default">Classic: Green</option>
             {/* <option value="background-98-sky-logo">98-Sky</option> */}
-            <option value="background-98-cougar">98-Cougar</option>
+            {/* <option value="background-98-cougar">98-Cougar</option> */}
             {/* <option value="background-98-leopard">98-Leopard</option>
             <option value="background-98-space">98-Space</option>
             <option value="background-98-ocean">98-Ocean</option>
             <option value="background-98-computer">98-Computer</option> */}
-            <option value="background-xp-classic">XP-Classic</option>
+            {/* <option value="background-xp-classic">XP-Classic</option> */}
           </>
         )}
       </select>
