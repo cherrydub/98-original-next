123c8ea74dc9066b3f536b436f970b5608aefb3c
cherrydub <cherrydub@gmail.com>
2023-08-31 19:57:16 +0100

    Start button active windows updated

--- src/app/BottomDesktopBar.jsx
+++ src/app/BottomDesktopBar.jsx
@@ -41,8 +41,7 @@ export default function BottomDesktopBar({
       addActiveComponent("Cv");
       addActiveComponent("Projects");
       addActiveComponent("Todo");
-      addActiveComponent("Memes");
-      // addActiveComponent("Contact");
+      addActiveComponent("Contact");
       addActiveComponent("Welcome");
     }
   };
